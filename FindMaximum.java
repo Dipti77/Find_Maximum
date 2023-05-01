136b9a611eabfe2b336a0e1f858b9409852a7bc0
Dipti77 <diptimhandlekar123@gmail.com>
2023-05-01 11:10:11 +0530

    Added: Find max of given 3 float

--- FindMaximum.java
+++ FindMaximum.java
@@ -1,9 +1,9 @@
 package com.bridgelabz;
 
 public class FindMaximum {
-
+	
 	public static Integer findMax(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
-
+		
 		Integer max = firstNumber;
 		
 		if (secondNumber.compareTo(max) > 0) {
@@ -15,14 +15,30 @@ public class FindMaximum {
 		}
 		return max;
 	}
+
+	public static float findMax(Float firstNumber, Float secondNumber, Float thirdNumber) {
+		
+		float max = firstNumber;
 	
-
-
+		if (secondNumber.compareTo(max) > 0) {
+			max = secondNumber;
+		}
+		
+		if (thirdNumber.compareTo(max) > 0) {
+			max = thirdNumber;
+		}
+		return max;
+	}
+		
+	
 	public static void main(String[] args) {
 		
 		int a;
-		a = findMax(12, 45, 32);
-		 
+		float b;
+		a = findMax(12, 45, 32); 
 		System.out.println("Maximum number is: " + a);
+		
+		b = findMax(10.2F, 5.6F, 8.4F);	
+		System.out.println("Maximum number is: " + b);
 	}
 }
