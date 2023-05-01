36faf2e9be90c5dfd5d95a26ec876c1be6c9057f
Dipti77 <diptimhandlekar123@gmail.com>
2023-05-01 11:16:33 +0530

    Added: Find max of given 3 string

--- FindMaximum.java
+++ FindMaximum.java
@@ -30,15 +30,33 @@ public class FindMaximum {
 		return max;
 	}
 		
+	public static String findMax(String firstNumber, String secondNumber, String thirdNumber) {
+		
+		String max = firstNumber;
+		
+		if (secondNumber.compareTo(max) > 0) {
+			max = secondNumber;
+		}
+		
+		if (thirdNumber.compareTo(max) > 0) {
+			max = thirdNumber;
+		}
+		return max;
+	} 
 	
 	public static void main(String[] args) {
 		
 		int a;
 		float b;
+		String c;
+		
 		a = findMax(12, 45, 32); 
 		System.out.println("Maximum number is: " + a);
 		
 		b = findMax(10.2F, 5.6F, 8.4F);	
 		System.out.println("Maximum number is: " + b);
+		
+		c = findMax("Dipti", "Sneha", "Niha");
+		System.out.println("Maximum number is: " + c);
 	}
 }
